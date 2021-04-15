f64111f42b1fbbeb3df2e5294110f7b6ff0672ce
jt-zhang <jtzhang6@gmail.com>
2021-04-15 15:07:28 +0800

    update

--- Overall/forest_&_power/generate_sql.py
+++ Overall/forest_&_power/generate_sql.py
@@ -5,7 +5,7 @@ from numpy.random import choice
 
 dictforest = {'forest':[ 'forest', 'Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points']}
 
-df_forest = pd.read_csv("/home/zhangjintao/Benchmark3/otherdataset/csvdata_sql/forest.csv", sep=',', escapechar='\\', encoding='utf-8', low_memory=False, quotechar='"')
+df_forest = pd.read_csv("../train-test-data/forest_&_power-data-sql/forest.csv", sep=',', escapechar='\\', encoding='utf-8', low_memory=False, quotechar='"')
 
 df_forest = df_forest.dropna(axis=0, how='any', inplace=False)
 
@@ -24,7 +24,7 @@ for key, value in dictforest.items():
     for i in range(1, len(value)):
         locals()[value[0] + '_' + value[i]] = list(locals()['df_'+key][value[i]].unique())
 
-f2 = open("/home/zhangjintao/Benchmark3/otherdataset/csvdata_sql/forest.sql",'w')
+f2 = open("../train-test-data/forest_&_power-data-sql/forest.sql",'w')
 
 ops = ['=', '<', '>']  #训练和测试均无 >=, <=
 
@@ -51,7 +51,7 @@ f2.close()
 
 dictpower = {'power':[ 'power', 'Global_active_power','Global_reactive_power','Voltage','Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3']}
 
-df_power = pd.read_csv("/home/zhangjintao/Benchmark3/otherdataset/csvdata_sql/power.csv", sep=',', escapechar='\\', encoding='utf-8', low_memory=False, quotechar='"')
+df_power = pd.read_csv("../train-test-data/forest_&_power-data-sql/power.csv", sep=',', escapechar='\\', encoding='utf-8', low_memory=False, quotechar='"')
 
 df_power = df_power.dropna(axis=0, how='any', inplace=False)
 
@@ -69,7 +69,7 @@ power_Horizontal_Distance_To_Fire_Points=[]
 for key, value in dictpower.items():
     for i in range(1, len(value)):
         locals()[value[0] + '_' + value[i]] = list(locals()['df_'+key][value[i]].unique())
-f2 = open("/home/zhangjintao/Benchmark3/otherdataset/csvdata_sql/power.sql",'w')
+f2 = open("../train-test-data/forest_&_power-data-sql/power.sql",'w')
 
 ops = ['=', '<', '>']  #训练和测试均无 >=, <=
 
@@ -94,9 +94,9 @@ for i in range(180000):
     f2.write(questr)
 f2.close()
 
-f = open('/home/zhangjintao/Benchmark3/otherdataset/csvdata_sql/' + 'forest' + 'test.sql')
+f = open('../train-test-data/forest_&_power-data-sql/' + 'forest' + 'test.sql')
 queries = f.readlines() 
-f2 = open('/home/zhangjintao/Benchmark3/otherdataset/csvdata_sql/' + 'forest' + 'test2.sql', 'w')
+f2 = open('../train-test-data/forest_&_power-data-sql/' + 'forest' + 'test2.sql', 'w')
 for query in queries:
     cnt = query.count('forest')
     if cnt == 3 or cnt == 12:
@@ -105,9 +105,9 @@ for query in queries:
         f2.write(query)
 f2.close()
 
-f = open('/home/zhangjintao/Benchmark3/otherdataset/csvdata_sql/' + 'power' + 'test.sql')
+f = open('../train-test-data/forest_&_power-data-sql/' + 'power' + 'test.sql')
 queries = f.readlines() 
-f2 = open('/home/zhangjintao/Benchmark3/otherdataset/csvdata_sql/' + 'power' + 'test2.sql', 'w')
+f2 = open('../train-test-data/forest_&_power-data-sql/' + 'power' + 'test2.sql', 'w')
 for query in queries:
     cnt = query.count('power.')
     if cnt == 1 or cnt == 10:
