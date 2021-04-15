efe216ab7801627fc4bbc1e0188a85e922a827d8
jt-zhang <jtzhang6@gmail.com>
2021-04-15 15:21:57 +0800

    update

--- Overall/learnedcardinalities-master/run.sh
+++ Overall/learnedcardinalities-master/run.sh
@@ -1,19 +1,6 @@
 cd mscn
-'python preprocessing.py --datasets-dir ./csvdata_sql --raw-query-file /home/zhangjintao/Benchmark3/sql_truecard/' + version + \
-    'train.sql' + ' --min-max-file /home/zhangjintao/Benchmark3/CardinalityEstimationBenchmark/learnedcardinalities-master/data/' + version + '_min_max_vals.csv ' + '--table ' + version + ' --alias cdcs'
-'python preprocessing.py --datasets-dir ./csvdata_sql --raw-query-file /home/zhangjintao/Benchmark3/sql_truecard/' + version + \
-    'test.sql' + ' --min-max-file /home/zhangjintao/Benchmark3/CardinalityEstimationBenchmark/learnedcardinalities-master/data/' + version + '_min_max_vals.csv ' + '--table ' + version + ' --alias cdcs'
+python preprocessing.py --datasets-dir ../train-test-data/imdbdata-num/ --raw-query-file ../train-test-data/imdb-cols-sql/4/train-4-num.sql --min-max-file ./data/column_min_max_vals.csv
+python preprocessing.py --datasets-dir ../train-test-data/imdbdata-num/ --raw-query-file ../train-test-data/imdb-cols-sql/4/test-only4-num.sql --min-max-file ./data/column_min_max_vals.csv
 cd ..
-python train.py --min-max-file /home/sunji/CardinalityEstimationBenchmark/learnedcardinalities-master/data/skew4_min_max_vals.csv --queries 10000 --epochs 100 --batch 1024 --hid 256 --train-query-file /home/sunji/CardinalityEstimationBenchmark/train-test-data/skew-sql/skew4/train-num.sql --test-query-file /home/sunji/CardinalityEstimationBenchmark/train-test-data/skew-sql/skew4/test-num.sql --train
-python train.py --min-max-file /home/sunji/CardinalityEstimationBenchmark/learnedcardinalities-master/data/skew4_min_max_vals.csv --queries 10000 --epochs 100 --batch 1024 --hid 256 --train-query-file /home/sunji/CardinalityEstimationBenchmark/train-test-data/skew-sql/skew4/train-num.sql --test-query-file /home/sunji/CardinalityEstimationBenchmark/train-test-data/skew-sql/skew4/test-num.sql
-
-'python preprocessing.py --datasets-dir ./csvdata_sql --raw-query-file /home/zhangjintao/Benchmark3/sql_truecard/' + version + \
-    'train.sql' + ' --min-max-file /home/zhangjintao/Benchmark3/CardinalityEstimationBenchmark/learnedcardinalities-master/data/' + version + '_min_max_vals.csv ' + '--table ' + version + ' --alias cdcs'
-'python preprocessing.py --datasets-dir ./csvdata_sql --raw-query-file /home/zhangjintao/Benchmark3/sql_truecard/' + version + \
-    'test.sql' + ' --min-max-file /home/zhangjintao/Benchmark3/CardinalityEstimationBenchmark/learnedcardinalities-master/data/' + version + '_min_max_vals.csv ' + '--table ' + version + ' --alias cdcs'
-'python train.py --min-max-file /home/zhangjintao/Benchmark3/CardinalityEstimationBenchmark/learnedcardinalities-master/data/' + version + \
-    '_min_max_vals.csv --queries 10000 --epochs 100 --batch 1024 --hid 256 --train-query-file /home/zhangjintao/Benchmark3/sql_truecard/' + version + \
-    'train.sql --test-query-file /home/zhangjintao/Benchmark3/sql_truecard/' + version + 'test.sql --train'
-'python train.py --min-max-file /home/zhangjintao/Benchmark3/CardinalityEstimationBenchmark/learnedcardinalities-master/data/' + version + \
-    '_min_max_vals.csv --queries 10000 --epochs 100 --batch 1024 --hid 256 --train-query-file /home/zhangjintao/Benchmark3/sql_truecard/' + version + \
-    'train.sql --test-query-file /home/zhangjintao/Benchmark3/sql_truecard/' + version + 'test.sql'
+python train.py --queries 10000 --epochs 100 --batch 1024 --hid 256 --train-query-file ../train-test-data/imdb-cols-sql/4/train-4-num.sql --test-query-file ../train-test-data/imdb-cols-sql/4/test-only4-num.sql --train
+python train.py --queries 10000 --epochs 100 --batch 1024 --hid 256 --train-query-file ../train-test-data/imdb-cols-sql/4/train-4-num.sql --test-query-file ../train-test-data/imdb-cols-sql/4/test-only4-num.sql
