e9261b486e5214cbfb752ec7e223df77dc5e854e
jt-zhang <jtzhang6@gmail.com>
2021-09-07 22:45:57 +0800

    Update

--- Overall/neurocard/scripts/download_imdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-mkdir -p datasets/job && pushd datasets/job
-wget -c http://homepages.cwi.nl/~boncz/job/imdb.tgz && tar -xvzf imdb.tgz && popd
-
-python scripts/prepend_imdb_headers.py
