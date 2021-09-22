3c0e2ea095a8fb24383fd883ed774d5f0904ade1
jt-zhang <jtzhang6@gmail.com>
2021-09-23 02:19:31 +0800

    Update

--- Overall/kde_python/kde.py
+++ Overall/kde_python/kde.py
@@ -6,6 +6,21 @@ from torch.autograd import Variable
 from torch.utils.data import TensorDataset
 import numpy as np
 
+import argparse
+
+parser = argparse.ArgumentParser(description='KDE')
+parser.add_argument('--train-file', type=str, help='datasets_dir',
+                    default='/home/jintao/CardinalityEstimationBenchmark/train-test-data/cols-sql/4/train-4-num.sql')
+parser.add_argument('--test-file', type=str, help='sqls to be parsed',
+                    default='/home/jintao/CardinalityEstimationBenchmark/train-test-data/cols-sql/4/test-only4-num.sql')
+parser.add_argument('--min-max-file', type=str, help='Min Max',
+                    default='/home/jintao/CardinalityEstimationBenchmark/learnedcardinalities-master/data/column_min_max_vals.csv')
+# parser.add_argument("--version", help="version", type=str, default='cols_4_distinct_1000_corr_5_skew_5')
+
+args = parser.parse_args()
+min_max_file = args.min_max_file
+trainpath = args.train_file
+testpath = args.test_file
 
 class KDE():
     def __init__(self, samples, num_attributes):
@@ -14,11 +29,11 @@ class KDE():
         self.sample = samples
         self.num_attributes = num_attributes
 
-    def train(self, train_predicates, cardinalities, total_card, batch_size, num_epochs):
+    def train(self, train_predicates, cardinalities, min_card, max_card, batch_size, num_epochs):
         num_sample = len(self.sample)
         predicates_lower = torch.FloatTensor([[pre[i] for i in range(0, len(pre), 2)] for pre in train_predicates])
         predicates_upper = torch.FloatTensor([[pre[i] for i in range(1, len(pre), 2)] for pre in train_predicates])
-        cardinalities = torch.FloatTensor(cardinalities) / total_card
+        cardinalities = (torch.FloatTensor(cardinalities)-min_card) / (max_card-min_card)
         samples = torch.FloatTensor(self.sample)
         optimizer = torch.optim.Adam([self.H], lr=0.001)
         total_size = len(train_predicates)
@@ -72,10 +87,10 @@ class KDE():
         print(f'Validate 99th Q-error: {np.percentile(qerrors, 99)}')
         print(f'Validate MSE: {mses.mean()}')
 
-    def test(self, test_predicates, cardinalities, total_card):
+    def test(self, test_predicates, cardinalities, min_card, max_card):
         predicates_lower = torch.FloatTensor([[pre[i] for i in range(0, len(pre), 2)] for pre in test_predicates])
         predicates_upper = torch.FloatTensor([[pre[i] for i in range(1, len(pre), 2)] for pre in test_predicates])
-        cardinalities = torch.FloatTensor(cardinalities) / total_card
+        cardinalities = (torch.FloatTensor(cardinalities)-min_card) / (max_card-min_card)
         samples = torch.FloatTensor(self.sample)
         test_dataset = TensorDataset(predicates_lower, predicates_upper, cardinalities)
         test_loader = torch.utils.data.DataLoader(test_dataset)
@@ -107,12 +122,77 @@ class KDE():
         print(f'99th Q-error: {np.percentile(qerrors, 99)}')
         print(f'MSE: {mses.mean()}')
 
+def prepare_pattern_workload(path):
+    Embed = []
+    truecard = []
+    minmax = pd.read_csv(min_max_file)
+    minmax = minmax.set_index('name')
+    min_card = 999999999999.0
+    max_card = 0.0
+    with open(path + '.csv', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            tables = sorted([x.split(' ')[1] for x in line.split('#')[0].split(',')])
+            local_cols = []
+            vecs = []
+            for col_name in minmax.index:
+                if col_name.split('.')[0] in tables:
+                    local_cols.append(col_name)
+                    vecs.append(0.0)
+                    vecs.append(1.0)
+            conds = [x for x in line.split('#')[2].split(',')]
+            for i in range(int(len(conds) / 3)):
+                attr = conds[i * 3]
+                op = conds[i * 3 + 1]
+                value = conds[i * 3 + 2]
+                idx = local_cols.index(attr)
+                maximum = float(minmax.loc[attr]['max'])
+                minimum = float(minmax.loc[attr]['min'])
+                distinct_num = minmax.loc[attr]['num_unique_values']
+                if op == '=':
+                    offset = (maximum - minimum) / distinct_num / 2.0
+                    upper = ((float(value) + offset) - minimum) / (maximum - minimum)
+                    lower = (float(value) - offset - minimum) / (maximum - minimum)
+                elif op == '<':
+                    upper = (float(value) - minimum) / (maximum - minimum)
+                    lower = 0.0
+                elif op == '>':
+                    upper = 1.0
+                    lower = (float(value) - minimum) / (maximum - minimum)
+                else:
+                    raise Exception(op)
+                if upper < vecs[idx * 2 + 1]:
+                    vecs[idx * 2 + 1] = upper
+                if lower > vecs[idx * 2]:
+                    vecs[idx * 2] = lower
+
+            card = float(line.split('#')[-1])
+            Embed.append(vecs)
+            truecard.append(card)
+            if card < min_card:
+                min_card = math.log(card)
+            if card > max_card:
+                max_card = math.log(card)
+    num_attributes = len(vecs)/2
+    return Embed, truecard, min_card, max_card, num_attributes
+
 
 if __name__ == '__name__':
     # 0.4 <= B <= 0.5
     # min(A) <= A <= max(A)
     # sql parser
-    train_predicates = [[0.1, 0.3, 0.4, 0.5],
+    batch_size = 128
+    num_epochs = 100
+    train_predicates, train_cardinalities, min_card, max_card, num_attributes = prepare_pattern_workload(trainpath)
+    sample = 
+    kde = KDE(samples=sample, num_attributes = num_attributes)
+    kde.train(train_predicates, train_cardinalities, min_card, max_card, batch_size, num_epochs)
+    # kde.train(train_predicates, train_cardinalities, total_card, batch_size, num_epochs)
+    
+    test_predicates, test_cardinalities, min_card, max_card, num_attributes = prepare_pattern_workload(testpath)
+    kde.test(test_predicates, test_cardinalities, min_card, max_card)
+
+    '''train_predicates = [[0.1, 0.3, 0.4, 0.5],
                         [0.3, 0.6, 0.7, 0.8],
                         [0.1, 0.4, 0.9, 1.0],
                         [0.2, 0.3, 0.7, 0.8],
@@ -133,3 +213,4 @@ if __name__ == '__name__':
     kde = KDE(samples=sample, num_attributes=num_attributes)
     kde.train(train_predicates, train_cardinalities, total_card, batch_size, num_epochs)
     kde.test(test_predicates, test_cardinalities, total_card)
+    '''
